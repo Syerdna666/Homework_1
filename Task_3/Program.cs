39a8d6150d1e74b7b53766362620da43c5d2dc3a
Graceless <syerdnaswag@gmail.com>
2022-08-19 18:43:51 +0300

    Added Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    System.Console.WriteLine($"{number} - чётное");
+}
+else
+{
+    System.Console.WriteLine($"Число {number} - нечётное");
+}
