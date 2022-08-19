185ef524f055b5993966eb09ab88c6e1991e2428
Graceless <syerdnaswag@gmail.com>
2022-08-19 18:33:31 +0300

    Added Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Введите первое число: ");
+int number_1 = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int number_2 = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите третье число: ");
+int number_3 = int.Parse(Console.ReadLine());
+int max_number = number_1;
+
+if (number_2 > number_1)
+{
+    max_number = number_2;
+}
+if (number_3 > max_number)
+{
+    max_number = number_3;
+}
+System.Console.WriteLine("Наибольшее из трех чисел: " + max_number);
