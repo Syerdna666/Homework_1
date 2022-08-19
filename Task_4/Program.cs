6e7b261a99e4263cf30abe3ee47656e8b33e053c
Graceless <syerdnaswag@gmail.com>
2022-08-19 21:05:22 +0300

    Added Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите N: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number > 1)
+{
+    for (int i = 2; i <= number; i += 2)
+    {
+        Console.Write(i + " ");
+    }
+}
+else
+{
+    System.Console.WriteLine("Введите значение N больше 1");
+}
