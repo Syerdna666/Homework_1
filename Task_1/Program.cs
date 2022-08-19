253155eff7d68aca2100d2c1c680c344c8c76881
Graceless <syerdnaswag@gmail.com>
2022-08-19 18:15:47 +0300

    Added Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Введите первое число: ");
+int number_1 = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int number_2 = int.Parse(Console.ReadLine());
+
+if (number_1 > number_2)
+{
+    System.Console.WriteLine($"Число {number_1} больше числа {number_2}");
+}
+else if (number_1 > number_2)
+{
+    System.Console.WriteLine($"Число {number_2} больше числа {number_1}");
+}
+else
+{
+    System.Console.WriteLine($"Число {number_1} равно числу {number_2}");
+}
